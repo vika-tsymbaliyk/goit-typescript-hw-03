e0d0723fda39d94fb3e411796c2a3358bae2c142
vika-tsymbaliyk <vika.bronytska@gmail.com>
2023-10-28 01:32:44 +0300

    done

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,49 @@
+class Key {
+    private signature: number = Math.random();
+    getSignature(): number{
+        return this.signature
+    }
+}
+
+class Person{
+    constructor(private key: Key) {}
+
+    getKey(): Key {
+      return this.key;
+    }
+}
+
+abstract class House {
+    protected door: boolean;
+    protected key: Key;
+    protected tenants: Person[] = [];
+  
+    constructor(key: Key) {
+      this.door = false;
+      this.key = key;
+    }
+
+    public comeIn(person: Person): void {
+        if (this.door) {
+        this.tenants.push(person);
+        }
+    }
+    abstract openDoor(key: Key): void;
+}
+
+
+class MyHouse extends House {
+    openDoor(key: Key): void {
+      if (key.getSignature() === this.key.getSignature()) {
+        this.door = true;
+      } else {
+        this.door = false;
+      }
+    }
+}
+
+
+
 const key = new Key();
 
 const house = new MyHouse(key);
